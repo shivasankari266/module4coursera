fe939ac247c29e4c6580aebbad023aee53a239b1
shivasankari266 <150837576+shivasankari266@users.noreply.github.com>
2025-03-29 12:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 // Hello Module
 var helloSpeaker = (function () {
-    var speakWord = "Hello";
+  var speakWord = "Hello";
 
-    function speak(name) {
-        var message = speakWord + " " + name;
-        document.write(message + "<br>");  // Display on webpage
-    }
+  function speak(name) {
+      var message = speakWord + " " + name;
+      document.getElementById("output").innerHTML += message + "<br>"; // Update the page
+  }
 
-    return {
-        speak: speak
-    };
+  return {
+      speak: speak
+  };
 })();
 
 // Goodbye Module
 var goodbyeSpeaker = (function () {
-    var speakWord = "Goodbye";
+  var speakWord = "Goodbye";
 
-    function speak(name) {
-        var message = speakWord + " " + name;
-        document.write(message + "<br>");  // Display on webpage
-    }
+  function speak(name) {
+      var message = speakWord + " " + name;
+      document.getElementById("output").innerHTML += message + "<br>"; // Update the page
+  }
 
-    return {
-        speak: speak
-    };
+  return {
+      speak: speak
+  };
 })();
 
 // Main Execution
 (function () {
-    var names = ["John", "Paul", "Jen", "Mary", "Jason", "Frank", "Larry", "Jim", "Liam"];
+  var names = ["John", "Paul", "Jen", "Mary", "Jason", "Frank", "Larry", "Jim", "Liam"];
 
-    for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
+  for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
 
-        if (firstLetter === 'j') {
-            goodbyeSpeaker.speak(names[i]);
-        } else {
-            helloSpeaker.speak(names[i]);
-        }
-    }
+      if (firstLetter === 'j') {
+          goodbyeSpeaker.speak(names[i]);
+      } else {
+          helloSpeaker.speak(names[i]);
+      }
+  }
 })();
